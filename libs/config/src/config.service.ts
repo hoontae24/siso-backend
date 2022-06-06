2f9ecf8d884cb05ff79dde9172dcbb3c40ff2bca
hoontae24 <kadr8941@gmail.com>
2022-06-06 16:23:42 +0900

    Chore: [SISO-42] 환경변수 관리용 lib 컴포넌트 추가

---
+++ libs/config/src/config.service.ts
@@ -0,0 +1,19 @@
+import { Injectable } from '@nestjs/common';
+
+@Injectable()
+export class ConfigService {
+  private readonly env: NodeJS.ProcessEnv;
+
+  constructor() {
+    // Initialize
+    // NOTE 인스턴스 생성 시 초기화한 후 동적으로 env가 변경되지 않도록 하기 위해서 로컬 멤버변수로 지정함.
+    this.env = process.env;
+  }
+
+  public get port(): number {
+    const { PORT } = this.env;
+    const port = (PORT && parseInt(PORT)) || 3000;
+    console.log('port', port);
+    return port;
+  }
+}
