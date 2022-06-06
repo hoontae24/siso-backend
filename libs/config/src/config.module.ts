50d69eae462b7c22292d8197e8508c01e3063137
hoontae24 <kadr8941@gmail.com>
2022-06-06 16:52:08 +0900

    Chore: [SISO-42] 환경변수를 로컬 환경에서 덮어쓰기

--- libs/config/src/config.module.ts
+++ libs/config/src/config.module.ts
@@ -2,8 +2,12 @@ import { Module } from '@nestjs/common';
 import { ConfigModule as NestConfigModule } from '@nestjs/config';
 import { ConfigService } from './config.service';
 
+const NestConfigDynamicModule = NestConfigModule.forRoot({
+  envFilePath: ['.env.local', '.env'],
+});
+
 @Module({
-  imports: [NestConfigModule.forRoot()],
+  imports: [NestConfigDynamicModule],
   providers: [ConfigService],
   exports: [ConfigService],
 })
