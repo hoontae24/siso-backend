16b15c15ca9a3b97456852dc2a9de3fadf0c027d
hoontae24 <kadr8941@gmail.com>
2022-06-11 18:15:07 +0900

    Feat: [SISO-55] Note 샘플 데이터 제공 로직 추가

--- src/notes/notes.service.ts
+++ src/notes/notes.service.ts
@@ -1,4 +1,14 @@
 import { Injectable } from '@nestjs/common';
+import { notes } from 'src/notes/fixtures/notes';
+import { Note } from 'src/notes/interfaces/notes';
 
 @Injectable()
-export class NotesService {}
+export class NotesService {
+  async findAll(): Promise<Note[]> {
+    return notes;
+  }
+
+  async findById(id: number): Promise<Note | null> {
+    return notes.find((note) => note.id === id) ?? null;
+  }
+}
