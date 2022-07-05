bfb05c21828ebb544677e006d741aae4012832a2
hoontae24 <kadr8941@gmail.com>
2022-07-05 18:10:55 +0900

    Fix: [SISO-73] 더미 데이터에 찬성/반대 투표수 추가

--- src/vote-subjects/vote-subjects.service.ts
+++ src/vote-subjects/vote-subjects.service.ts
@@ -17,17 +17,23 @@ const dummy = [
     title: '테스트 제목 1',
     agreeDescription: '찬성a',
     oppositeDescription: '반대a',
+    voteAgreeCount: 1590,
+    voteOppositeCount: 590,
   },
   {
     id: 'b',
     title: '테스트 제목 2',
     agreeDescription: '찬성b',
     oppositeDescription: '반대b',
+    voteAgreeCount: 159080,
+    voteOppositeCount: 10,
   },
   {
     id: 'c',
     title: '테스트 제목 3',
     agreeDescription: '찬성c',
     oppositeDescription: '반대c',
+    voteAgreeCount: 12,
+    voteOppositeCount: 0,
   },
 ];
