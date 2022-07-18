244e5b4cf5e1c6ee765357d286a57e9eb5f0255a
hoontae24 <kadr8941@gmail.com>
2022-07-19 00:02:35 +0900

    Feat: [SISO-85] 투표 정보 API에 문서화 적용

--- src/vote-subjects/vote-subjects.controller.ts
+++ src/vote-subjects/vote-subjects.controller.ts
@@ -1,11 +1,17 @@
 import { Controller, Get, NotFoundException, Param } from '@nestjs/common';
+import { ApiOperation, ApiTags } from '@nestjs/swagger';
 import { VoteSubjectsService } from './vote-subjects.service';
 
 @Controller('vote-subjects')
+@ApiTags('투표 주제 API')
 export class VoteSubjectsController {
   constructor(private readonly voteSubjectService: VoteSubjectsService) {}
 
   @Get()
+  @ApiOperation({
+    summary: '투표 주제 목록 조회',
+    description: '투표 주제를 목록 형태로 조회한다.',
+  })
   async findAll() {
     const voteSubjects = await this.voteSubjectService.findAll();
 
@@ -15,6 +21,10 @@ export class VoteSubjectsController {
   }
 
   @Get(':id')
+  @ApiOperation({
+    summary: '투표 주제 조회',
+    description: '특정 ID에 대한 투표 주제를 조회한다.',
+  })
   async findById(@Param('id') id: string) {
     const voteSubject = await this.voteSubjectService.findById(id);
     if (!voteSubject) {
